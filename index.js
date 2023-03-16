33842ff00d7a64146b1e8fc5d8f9cdeaaa1bca86
Alden Reyes <122179486+AldenReyes@users.noreply.github.com>
2023-03-15 19:54:24 -0700

    Completed Lab; explored different control flow statements

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(num) {
+  if (num > 2500) {
+    return 'No can do.';
+  }
+  if (num > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  if (num > 400 && num <= 2000) {
+    return 'That will be twenty bucks.';
+  }
+  if (num <= 400) {
+    return 'This one is on me!';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityName) {
+  return cityName === 'NYC' ? ('Ok, sounds good.') : ('No go.');
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipAmount) {
+  switch (tipAmount) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.'
+  }
 }
